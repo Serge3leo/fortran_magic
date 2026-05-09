24b6d146378decd7a1e3adaa8fcc69f89d41b88d
Serguei E. Leontiev <leo@sai.msu.ru>
2026-05-10 01:51:06 +0300

    Debug end (fail)

--- .pre-commit-ruff.sh
+++ .pre-commit-ruff.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
-# vim:set sw=4 ts=8 fileencoding=utf8:
-# SPDX-License-Identifier: BSD-2-Clause
-# SPDX-FileCopyrightText: 2025 Сергей Леонтьев (leo@sai.msu.ru)
 
 set -e
 uv run --group dev ruff check .
